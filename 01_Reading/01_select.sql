6f80ef989693224e0bb0b4cabc716c4fd4a2fbdc
Alejandro-cj <aloflow_345@hotmail.com>
2024-05-08 13:00:32 -0500

    select

---
+++ 01_Reading/01_select.sql
@@ -0,0 +1,14 @@
+/*
+SELECT
+*/
+--Obtine todos los datos de la tabla "users"
+
+SELECT * FROM users;
+
+-- Obtiene todos los nombre de la tabla "users"
+
+SELECT name FROM users;
+
+-- Obtiene todos los identificadores y nombre de la tabla "users"
+
+SELECT user_id, name FROM users;
