68de3f8cd6605b5fc7a4980c4b59da2625a41915
Alejandro-cj <aloflow_345@hotmail.com>
2024-05-08 15:37:33 -0500

    count

---
+++ 10_count.sql
@@ -0,0 +1,5 @@
+--cuenta cuantas fallas contiene la tabla "users"
+SELECT COUNT(*) FROM users;
+
+--cuenta cuntas filas contiene un dato no nulo en el campo edad de la tabla "users"
+SELECT COUNT(age) FROM users;
