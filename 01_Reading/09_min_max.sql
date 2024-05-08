ea50d9b2ef7d98ddce91ab6acf232350830f9eac
Alejandro-cj <aloflow_345@hotmail.com>
2024-05-08 15:34:59 -0500

    MIN-MAX

---
+++ 01_Reading/09_min_max.sql
@@ -0,0 +1,5 @@
+-- obtiene el valor menor del campo edad de la tabla "users"
+SELECT MIN(age) FROM users;
+
+-- obtiene el valor mayor del campo edad de la tabla "users"
+SELECT MAX(age) FROM users;
